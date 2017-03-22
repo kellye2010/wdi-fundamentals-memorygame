501157a413f71341ef7ab306dfbaf9b6bc97b9a9
Kelly English <thekellyenglish@gmail.com>
2017-03-22 13:55:36 -0400

    Add files via upload
    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,30 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+alert('Hello, friends.');
+
+// var cardOne = "queen"; var cardTwo = "king"; var cardThree = "queen"; var cardFour = "king";
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay =[];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay[0]);
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[1])
+//console.log("User flipped " + cardFour);
+
+if (cardsInPlay.length === 2) {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("Congratulations! You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
