81dde669ad9e90590152d841027d8142c01499d9
Kelly English <thekellyenglish@gmail.com>
2017-03-23 01:09:07 -0400

    Add files via upload
    Unit 9 Assignment
    (cardsInPlay array issue - it is empty)

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,4 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 
 alert('Hello, friends.');
 
@@ -7,24 +7,44 @@ alert('Hello, friends.');
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay =[];
 
-var cardOne = cards[0];
+//var cardOne = cards[0];
+//cardsInPlay.push(cardOne);
+//console.log("User flipped " + cardsInPlay[0]);
 
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardsInPlay[0]);
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[1])
+//var cardTwo = cards[2];
+//cardsInPlay.push(cardTwo);
+//console.log("User flipped " + cardsInPlay[2])
 //console.log("User flipped " + cardFour);
 
-if (cardsInPlay.length === 2) {
-
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("Congratulations! You found a match!");
-	} else {
-		alert("Sorry, try again.")
-	}
+//if (cardsInPlay.length === 2) {
+	//if (cardsInPlay[0] === cardsInPlay[2]) {
+	//	alert("Congratulations! You found a match!");
+	//} else {
+	//	alert("Sorry, try again.")
+	//}
+//}
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
 }
+
+
+
+var flipCard = function (cardId) {
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("User flipped " + cards[cardId]);
+		cardsInPlay.push(cards[cardId]);
+		alert(checkForMatch);
+	} else {
+		alert(checkForMatch);
+	}
+  }
+};
+
+flipCard(0);
+flipCard(2);
+//cardsInPlay.push(cards[cardId]);
